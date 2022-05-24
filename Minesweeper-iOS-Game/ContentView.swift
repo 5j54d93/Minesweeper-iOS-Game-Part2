3c23304af2e447a42dd56963c55d15bcd72ac10f
Ricky Chuang <songsongyyohya@gmail.com>
2022-05-24 15:30:08 +0800

    Initial Commit

---
+++ Minesweeper-iOS-Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Minesweeper-iOS-Game
+//
+//  Created by 莊智凱 on 2022/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Minesweeper-iOS-Game/Minesweeper_iOS_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minesweeper_iOS_GameApp.swift
+//  Minesweeper-iOS-Game
+//
+//  Created by 莊智凱 on 2022/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Minesweeper_iOS_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
